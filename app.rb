77a84762139ab4456596c2042116db47776392ab
Marissa Zhong <mwz2101@columbia.edu>
2018-07-13 19:09:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,9 +15,7 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    while @number > 0 do
-      "#{@phrase}"
-      @number -= 1
-    end
+    @result = ""
+    @number.times do @result += @phrase end
   end
 end
