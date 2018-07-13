b7a48b0736f77a875ff079b5e352ec0a6fa71c09
Marissa Zhong <mwz2101@columbia.edu>
2018-07-13 19:15:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -25,7 +25,16 @@ class App < Sinatra::Base
   end
 
   get "/:operation/:number1/:number2" do
-    
+    case params[:operation]
+    when "add"
+      "#{params[:number1].to_i + params[:number2].to_i}"
+    when "subtract"
+
+    when "multiply"
+
+    when "divide"
+
+    end
   end
 
 end
