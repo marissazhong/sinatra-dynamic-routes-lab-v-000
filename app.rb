31abfea787e9242422e1cd861b0dacc78875d5ef
Marissa Zhong <mwz2101@columbia.edu>
2018-07-13 19:11:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,5 +20,7 @@ class App < Sinatra::Base
     "#{@result}"
   end
 
-  get "/say/:word1/:word2/:word3/:word4/:word5"
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    @result = params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5] +
+  end
 end
