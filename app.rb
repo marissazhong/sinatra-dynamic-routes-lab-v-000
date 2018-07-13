c78b7a68e9b98abf63dbb383d9ad3e14e0b47f4e
Marissa Zhong <mwz2101@columbia.edu>
2018-07-13 19:13:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ class App < Sinatra::Base
   end
 
   get "/:operation/:number1/:number2" do
-    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+    
   end
 
 end
